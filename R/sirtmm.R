3fb7ed9dd8c23fdcbfe1aeb17a1cf98390b1a37e
Yikai Lu <luyikei.qmltu@gmail.com>
2023-03-24 06:36:46 -0400

    anova: model selection

--- R/sirtmm.R
+++ R/sirtmm.R
@@ -6,7 +6,7 @@ sirtmm <- function(data,
                    max_emsteps = 10,
                    max_nriter = 50,
                    lambda1 = 1,
-                   lamnda2 = 2,
+                   lambda2 = 2,
                    ngs = 1,
                    verbose = FALSE) {
   rule <- fastGHQuad::gaussHermiteData(quadpts)
@@ -19,14 +19,52 @@ sirtmm <- function(data,
     k <- max(data)
     if (verbose) message("Using the maximum of the response matrix: ", k)
   }
-  
+
   extended <- ifelse(itemtype == "SIRT-MMe", TRUE, FALSE)
 
-  res <- EMSteps(data, k, points, weights, lambda1, lamnda2, ngs, max_emsteps, max_nriter, verbose, extended)
-  cs <- if (itemtype == "SIRT-MMe") res[["c"]] else rep(1/k, ncol(data))
-  se <- SE(data, res[["b"]],  res[["a"]], cs, res[["g"]], k, points, weights, extended)
-  return(list(
+  res <- EMSteps(data, k, points, weights, lambda1, lambda2, ngs, max_emsteps, max_nriter, verbose, extended)
+  if (itemtype == "SIRT-MM") res[["c"]] <-  rep(1/k, ncol(data))
+  se <- SE(data, res[["b"]],  res[["a"]], res[["c"]], res[["g"]], k, points, weights, extended)
+
+  mod <- list(
+    data = X,
+    options = list(
+      k = k,
+      itemtype = itemtype,
+      quadpts = quadpts,
+      tol = tol,
+      max_emsteps = max_emsteps,
+      max_nriter = max_nriter,
+      lambda1 = lambda1,
+      lambda2 = lambda2,
+      ngs = ngs
+    ),
+    quadpts = list(
+      weights = weights,
+      points = points
+    ),
     itempar = as.data.frame(res),
+    g.mat = res[["g"]],
     se = as.data.frame(se)
-  ))
+  )
+  class(mod) <- "sirtmmModel"
+  return(mod)
+}
+
+anova.sirtmmModel <- function(...) {
+  ret <- data.frame()
+  models <- list(...)
+  for (mod in models) {
+    eret <- EStep(mod$data, mod$itempar$b, mod$itempar$a, mod$itempar$c, mod$g.mat, mod$options$k,
+                  mod$quadpts$points, mod$quadpts$weights)
+    logLik <- LogLikliTotal(mod$data, mod$itempar$b, mod$itempar$a, mod$itempar$c, mod$g.mat, mod$options$k,
+                            eret[["rhat"]], mod$quadpts$points)
+    npar <- dim(mod$itempar)[1] * dim(mod$itempar)[2]
+    if (mod$options$itemtype != "SIRT-MMe") npar <- npar - dim(mod$itempar)[1]
+    ret <- rbind(ret, data.frame(
+      BIC=c(npar * log(nrow(mod$data)) - 2 * logLik),
+      logLik=c(logLik))
+    )
+  }
+  return(ret)
 }
